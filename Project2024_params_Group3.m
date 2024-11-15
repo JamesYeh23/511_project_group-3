0a491a227b391462f429d1862c9580e44b7c50bd
James Yeh <jamesyeh23@g.ucla.edu>
2024-11-15 13:15:59 -0800

    chore: Adding Pharmacokinetic subsystem and parameter file

---
+++ Project2024_params_Group3.m
@@ -0,0 +1,15 @@
+SV = 0.097; % L
+RAP = 5; % mmHg
+alpha = 0.01; % mmHg^-1
+beta = 0.01; % mmHg^-1
+tau_1 = 0.01; % hr
+tau_2 = 0.01; % hr
+T_D = 0.002; % hr
+MAP_eq = 90; % mmHg
+HR_eq = 70; % bpm
+TPR_eq = 12.5; % Resistance units
+T = 10; % hr
+V_c = 10; % L
+gamma = 0.25; % hr^-1
+D = 3500; % ug
+m = -0.09; % RU L ug^-1
